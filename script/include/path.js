5df7500a5b4e1432a2f3f55046648b0b3e750425
jarlyyn <jarlyyn@qq.com>
2021-09-10 02:16:48 +0800

    update

--- script/include/path.js
+++ script/include/path.js
@@ -1,8 +1,7 @@
 (function(){
     let Step=Include("include/step.js")()
     let Path=function(){
-        this.Arrived=0
-        this.Pending=0
+        this.Moving=[]
         this.Steps=[]
     }
     Path.prototype.Push=function(command,target){
@@ -20,26 +19,22 @@
             this.Steps[this.Steps.length-1].SetTarget(target)
         }
     }
-    Path.prototype.Empty=function(){
-        return this.Steps.length==0
-    }
+
     Path.prototype.Current=function(){
         if (this.Steps.length==0){
             return null
         }
         return this.Steps[0]
     }
-    Path.prototype.Allocate=function(){
+    Path.prototype.Move=function(){
         let step=this.Steps.shift()
         if (step){
-            this.Ejected++
+            this.Moving.push(step)
         }
-        return step
+        return this.Current()
     }
     Path.prototype.Arrive=function(){
-        this.Arrived++
-        this.Pending--
-        return this.Pending
+        return this.Moving.shift()
     }
     return Path
 })
