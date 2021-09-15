716155d3ebc0f094e36970578c6649af47f95111
jarlyyn <jarlyyn@qq.com>
2021-09-16 02:12:57 +0800

    update

--- script/core/prepare/prepare.js
+++ script/core/prepare/prepare.js
@@ -1,6 +1,11 @@
 (function (app) {
     let Prepare=function(id){
-
+        if (!id) {
+            throw "Prepare的id不能为空"
+        }
+        this.Check=function(){
+            return false
+        }
     }
     return Prepare
 })(App)
