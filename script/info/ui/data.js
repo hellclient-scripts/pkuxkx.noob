ac18a29c22eb8974b77240e7e57e096fc77f1ad4
jarlyyn <jarlyyn@qq.com>
2021-09-24 00:15:03 +0800

    update

--- script/info/ui/data.js
+++ script/info/ui/data.js
@@ -1,4 +1,5 @@
 (function(app){
+    const pagesize=10
     app.InfoUIDataShowList=function() {
         var list=Userinput.newlist("请选择你要管理的数据","请选择你要管理的数据")
         list.append("builtinrooms","内建房间信息(只读)")
@@ -10,9 +11,59 @@
     }
     app.InfoUIData=function(name,id,code,data) {
         if (code==0 && data){
-            app.ExecuteTask("queue","#to "+data)
+            switch (data){
+                case "builtinrooms":
+                    BuiltinroomsGrid.setpage(1)
+                    BuiltinroomsGrid.setfilter("")
+                    publishgrid(BuiltinroomsGrid,app.Info.BuiltinRooms)
+                break
+            }
         }
     }
+    
     app.RegisterAssistant("data","数据管理",app.InfoUIDataShowList,50)
+    let BuiltinroomsGrid=Userinput.newdatagrid("内建房间信息","内建房间信息管理")
+    let publishgrid=function(grid,alldata){
+        let page=grid.getpage()
+        let filter=grid.getfilter()
+        let start=(page-1)*pagesize
+        let end=page*pagesize
+        let count=0
+        grid.resetitems()
+        for (let i=0;i<alldata.length;i++){
+            let data=alldata[i]
+            if (filter && data.indexOf(filter)<0){
+                continue
+            }
+            count++
+            if (i>=start && i<end){
+                grid.append(i,alldata[i])
+            }
+        }
+        grid.setmaxpage(Math.ceil(count/pagesize))
+        grid.publish("")
+    }
+    BuiltinroomsGrid.setonpage("App.InfoUIDataBuiltinroomsGridOnPage")
+    app.InfoUIDataBuiltinroomsGridOnPage=function(name,id,code,data){
+        if (code==0 && data){
+            BuiltinroomsGrid.setpage(data-0)
+            publishgrid(BuiltinroomsGrid,app.Info.BuiltinRooms)
+        }
+    }
+    BuiltinroomsGrid.setonfilter("App.InfoUIDataBuiltinroomsGridOnFilter")
+    app.InfoUIDataBuiltinroomsGridOnFilter=function(name,id,code,data){
+        if (code==0 && data){
+            BuiltinroomsGrid.setpage(1)
+            BuiltinroomsGrid.setfilter(data)
 
+            publishgrid(BuiltinroomsGrid,app.Info.BuiltinRooms)
+
+        }
+    }
+    BuiltinroomsGrid.setonview("App.InfoUIDataBuiltinroomsGridOnView")
+    app.InfoUIDataBuiltinroomsGridOnView=function(name,id,code,data){
+        if (code==0 && data){
+            Userinput.alert("","查看内建房间",app.Info.BuiltinRooms[data-0])
+        }
+    }
 })(App)
