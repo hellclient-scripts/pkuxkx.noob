7554582dec38bd10bb8eab74530d217380617109
jarlyyn <jarlyyn@qq.com>
2021-10-19 02:16:11 +0800

    update

--- script/core/offer.js
+++ script/core/offer.js
@@ -1,9 +1,29 @@
-(function(app){
-    app.Offers={}
-    app.RegisterOffer=function(offer){
-        if (offer.ID==""){
-            throw "Task id不可为空"
+(function (app) {
+    app.Offers = {}
+    app.OfferGroups = {}
+    app.RegisterOffer = function (offer) {
+        if (offer.ID == "") {
+            throw "Offer id不可为空"
         }
-        app.Offers[offer.ID]=offer
-      }
+        app.Offers[offer.ID] = offer
+    }
+    app.RegisterOfferGroup=function(groupid,offers){
+        app.Offers[groupid]=offers
+    }
+    app.EvaluateOfferGroup = function (groupid, onfinish, onfail) {
+        let group=app.OfferGroups[groupid]
+        if (!group){
+            throw "要约组 ["+groupid+"] 没找到"
+        }
+        app.EvaluateOffers(group)
+    }
+    app.EvaluateOffers = function (offers, onfinish, onfail) {
+        for (var i in offers){
+            if (offers[i].Evaluate()){
+                offers[i].Accept(onfinish)
+                return 
+            }
+        }
+        app.ExecuteCallback(onfail)
+    }
 })(App)
