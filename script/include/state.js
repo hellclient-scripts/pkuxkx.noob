db9bec44526255ddb11163a8af440ce25ed3a729
jarlyyn <jarlyyn@qq.com>
2021-10-23 01:25:39 +0800

    update

--- script/include/state.js
+++ script/include/state.js
@@ -1,16 +1,13 @@
 (function(){
     var State=function(id){
         this.ID=id
-        this.Command=command
+        this.Tags={}
     }
-    State.prototype.Enter=function(oldstatue){
-        
+    State.prototype.Enter=function(context,oldstatue){
     }
-    State.prototype.Leave=function(newstatue){
-        
+    State.prototype.Leave=function(context,newstatue){
     }
-    State.prototype.OnEvent=function(event){
-        
+    State.prototype.OnEvent=function(context,event,data){
     }
     return State
 })()
---
+++ script/include/statecontext.js
@@ -0,0 +1,20 @@
+(function(){
+    var StateContext=function(){
+        this.State=null
+    }
+    StateContext.prototype.ChangeState=function(newstatue){
+        let old=this.State
+        if (this.State){
+            this.State.Leave(this,newstatue)
+        }
+        this.State=newstatue
+        this.State.Enter(this,old)
+    }
+    StateContext.prototype.OnEvent=function(event,data){
+        if (this.State){
+            this.State.OnEvent(this,event,data)
+        }
+    }
+    
+    return StateContxt
+})()
