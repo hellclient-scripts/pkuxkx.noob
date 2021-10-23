ec7127f02f58c6fecf1fd2572f37fe2242d02d3d
jarlyyn <jarlyyn@qq.com>
2021-10-24 01:48:53 +0800

    update

--- script/core/state/statemove.js
+++ script/core/state/statemove.js
@@ -4,11 +4,13 @@
         basicstate.call(this)
     }
     StateMove.prototype = Object.create(basicstate.prototype)
-    StateMove.prototype.Enter=function(){
-
+    StateMove.prototype.Enter=function(context,newstatue){
+        world.EnableTriggerGroup("move",true)
     }
-    StateMove.prototype.Leave=function(){
-        
+    StateMove.prototype.Leave=function(context,oldstatue){
+        basicstate.prototype.Leave.call(this,context,oldstatue)
+        world.EnableTimer("steptimeout",false)
+        world.EnableTriggerGroup("move",false)
     }
     StateMove.prototype.OnEvent=function(context,event,data){
     }
--- script/core/state/statepatrol.js
+++ script/core/state/statepatrol.js
@@ -5,11 +5,11 @@
         this.ID="patrol"
     }
     StatePatrol.prototype = Object.create(statemove.prototype)
-    StatePatrol.prototype.Enter=function(Context,newstatue){
-        statemove.prototype.Enter(Context,newstatue)
+    StatePatrol.prototype.Enter=function(context,newstatue){
+        statemove.prototype.Enter(context,newstatue)
     }
-    StatePatrol.prototype.Leave=function(Context,oldstatue){
-        statemove.prototype.Leave(Context,oldstatue)
+    StatePatrol.prototype.Leave=function(context,oldstatue){
+        statemove.prototype.Leave(context,oldstatue)
     }
     StatePatrol.prototype.OnEvent=function(context,event,data){
     }
