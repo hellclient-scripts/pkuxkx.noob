3063723c5541c574890e1e310e857a014de98a33
jarlyyn <jarlyyn@qq.com>
2021-11-17 02:56:35 +0800

    update

--- script/core/state/statearrive.js
+++
@@ -1,8 +0,0 @@
-(function (app) {
-    let basicstate = Include("core/state/basicstate.js")
-    let StateArrive=function(){
-        basicstate.call(this)
-        this.ID="arrive"
-    }
-    StateArrive.prototype = Object.create(basicstate.prototype)
-})(App)
