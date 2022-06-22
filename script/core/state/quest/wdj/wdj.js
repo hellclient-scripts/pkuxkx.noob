72ad258b312cc14c5658e3d858ab5c4e0ceb1513
jarlyyn <jarlyyn@qq.com>
2022-06-23 01:36:52 +0800

    update

--- script/core/state/quest/wdj/wdj.js
+++ script/core/state/quest/wdj/wdj.js
@@ -1,56 +1,86 @@
 (function (App) {
     let basicstate = Include("core/state/basicstate.js")
-    let State=function(){
+    let State = function () {
         basicstate.call(this)
-        this.ID="core.state.quest.wdj.wdj"
+        this.ID = "core.state.quest.wdj.wdj"
     }
-    let Start=function(){
-        let output=App.Data.Ask.Lines[0]
-        if (output.Words.length<2){
+    let Start = function (snap) {
+        let output = App.Data.Ask.Lines[0]
+        if (output.Words.length < 2) {
             App.Fail()
-            return 
+            return
         }
-        switch (output.Words[1].Text){
+        switch (output.Words[1].Text) {
             case "说道：「你去提水浇菜地。":
             case "说道：「我不是给过你任务了吗？提水浇菜地，快去！":
-                App.Quest.WDJ.Jiaoshui.Start()
-            break
+                App.Quest.WDJ.Type = "jiaoshui"
+                break
             case "说道：「你去提水浇田。":
-                case "说道：「我不是给过你任务了吗？提水浇田，快去！":
-                    App.Quest.WDJ.Jiaoshui.Start2()
+            case "说道：「我不是给过你任务了吗？提水浇田，快去！":
+                App.Quest.WDJ.Type = "jiaoshui2"
                 break
             case "说道：「你去把睡房的便桶全部倒进茅房。":
             case "说道：「我不是给过你任务了吗？把睡房的便桶全部倒进茅房，快去！":
-                App.Quest.WDJ.Maofang.Start()
-            break
+                App.Quest.WDJ.Type = "maofang"
+                break
             case "说道：「你去弄点烂菜叶喂猪。":
             case "说道：「我不是给过你任务了吗？弄点烂菜叶喂猪，快去！":
-                App.Quest.WDJ.Weizhu.Start()
-            break
+                App.Quest.WDJ.Type = "weizhu"
+                break
             case "说道：「你去把书房书架上的书整理一下。":
             case "说道：「我不是给过你任务了吗？把书房书架上的书整理一下，快去！":
-                App.Quest.WDJ.Zhengli.Start()
+                App.Quest.WDJ.Type = "zhengli"
                 break
             default:
                 App.Fail()
                 return
         }
+        App.Core.Snapshot.Rollback(snap)
+        App.Core.Puzzle.Show("wdj.start", "选择任务", "选择下一步的操作", DumpOutput(1), App.Quest.WDJ.Items)
+
     }
     State.prototype = Object.create(basicstate.prototype)
-    State.prototype.Enter=function(context,oldstatue){
+    State.prototype.OnEvent = function (context, event, data) {
+        switch (event) {
+            case "puzzle.answer":
+                if (App.Data.Puzzle.Answer == App.Quest.WDJ.Type) {
+                    switch (App.Data.Puzzle.Answer) {
+                        case "jiaoshui":
+                            App.Quest.WDJ.Jiaoshui.Start()
+                            return;
+                        case "jiaoshui2":
+                            App.Quest.WDJ.Jiaoshui.Start2()
+                            return
+                        case "maofang":
+                            App.Quest.WDJ.Maofang.Start()
+                            return
+                        case "weizhu":
+                            App.Quest.WDJ.Weizhu.Start()
+                            return
+                        case "zhengli":
+                            App.Quest.WDJ.Zhengli.Start()
+                            return
+                    }
+                }
+                App.Fail()
+        }
+    }
+    State.prototype.Enter = function (context, oldstatue) {
+        let snap = App.Core.Snapshot.Take("start")
+        App.Quest.WDJ.Type = ""
         App.Commands([
-            App.NewCommand("to",App.Options.NewWalk("wdj")),
-            App.NewCommand("patrol",App.Options.NewPath("n")),
-            App.NewCommand("do","drink"),
-            App.NewCommand("patrol",App.Options.NewPath("s;su")),
-            App.NewCommand("do","have"),
-            App.NewCommand("patrol",App.Options.NewPath("nd")),
-            App.NewCommand("ask",App.Quest.WDJ.QuestionJob),
-            App.NewCommand("function",Start),
+            App.NewCommand("to", App.Options.NewWalk("wdj")),
+            App.NewCommand("patrol", App.Options.NewPath("n")),
+            App.NewCommand("do", "drink"),
+            App.NewCommand("patrol", App.Options.NewPath("s;su")),
+            App.NewCommand("do", "have"),
+            App.NewCommand("patrol", App.Options.NewPath("nd")),
+            App.NewCommand("ask", App.Quest.WDJ.QuestionJob),
+            App.NewCommand("function", function () { Start(snap) }),
         ]).Push()
         App.Next()
     }
-    State.prototype.Leave=function(context,oldstatue){
+    State.prototype.Leave = function (context, oldstatue) {
         Userinput.hideall()
     }
     return State
--- script/quest/wdj/wdj.js
+++ script/quest/wdj/wdj.js
@@ -5,7 +5,15 @@
     App.Load("quest/wdj/jiaoshui/jiaoshui.js")
     App.Load("quest/wdj/maofang/maofang.js")
     App.Load("quest/wdj/zhengli/zhengli.js")
-
+    App.Quest.WDJ.Items=[
+        App.Core.Puzzle.NewItem("weizhu","喂猪任务"),
+        App.Core.Puzzle.NewItem("jiaoshui","浇菜任务"),
+        App.Core.Puzzle.NewItem("jiaoshui2","浇田任务"),
+        App.Core.Puzzle.NewItem("maofang","茅房任务"),
+        App.Core.Puzzle.NewItem("zhengli","整理书架任务"),
+    
+    ]
+    
     App.Quest.WDJ.Start=function(){
         App.Push(["core.state.quest.wdj.wdj"])
         App.Next()
