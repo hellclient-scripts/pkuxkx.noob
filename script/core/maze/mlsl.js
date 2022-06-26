541c11748a26fe5698d940fb4df42fba2dde368a
jarlyyn <jarlyyn@qq.com>
2022-06-26 21:13:47 +0800

    update

--- script/core/maze/mlsl.js
+++ script/core/maze/mlsl.js
@@ -1,29 +1,42 @@
 (function (App) {
-    let basicmaze=Include("include/maze.js")
-    let Maze=function(param){
-        basicmaze.call(this,param)
-        this.ID="苗岭山林"
+    let basicmaze = Include("include/maze.js")
+    let Maze = function (param) {
+        basicmaze.call(this, param)
+        this.ID = "苗岭山林"
     }
-    let exitsw=["wu","w","wd"]
-    let exitse=["eu","e","ed"]
+    let exitsw = ["wu", "w", "wd"]
+    let exitse = ["eu", "e", "ed"]
+    let exitsn = ["nu", "n", "nd"]
+    let exitss = ["su", "s", "sd"]
     Maze.prototype = Object.create(basicmaze.prototype)
-    Maze.prototype.IsEscaped=function(move){
-        return App.Data.Room.Name!="苗岭山林"
+    Maze.prototype.IsEscaped = function (move) {
+        return App.Data.Room.Name != "苗岭山林"
     }
-    Maze.prototype.Explore=function(move){
-        let dir=(this.Current=="西")?exitsw:exitse
-        if (App.HasRoomExit("out")){
-            App.Go("out")
-            return 
+    Maze.prototype.Dir=function(){
+        switch (this.Current) {
+            case "西":
+                return exitsw
+            case "北":
+                return exitsn
+            case "南":
+                return exitss
+            default:
+                return exitse
         }
-        App.Go(RandomList(dir))
     }
-    Maze.prototype.OnStateEvent=function(move,state,event,data){
-        switch (event){
+    Maze.prototype.Explore = function (move) {
+        if (App.HasRoomExit("out")) {
+            App.Go("out")
+            return
+        }
+        App.Go(RandomList(this.Dir()))
+    }
+    Maze.prototype.OnStateEvent = function (move, state, event, data) {
+        switch (event) {
             case "move.notallowed":
-                let dir=(this.Current=="西")?exitsw:exitse
-                let exit=RandomList(dir)
-                world.DoAfterSpecial(1, 'App.Go("'+exit+'")', 12);
+                let dir = this.Dir()
+                let exit = RandomList(dir)
+                world.DoAfterSpecial(1, 'App.Go("' + exit + '")', 12);
                 return true
                 break;
         }
