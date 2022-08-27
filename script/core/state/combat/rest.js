a5b9770fcbd7291649a560168517a816ba7a2a45
jarlyyn <jarlyyn@qq.com>
2022-08-27 23:05:27 +0800

    update

--- script/core/state/combat/rest.js
+++ script/core/state/combat/rest.js
@@ -13,6 +13,8 @@
             App.Send("dazuo max;dazuo 10")
         }else if (App.Core.PerQixue()<App.GetNumberParam("heal_below")){
             App.Send("do 10 yun heal")
+        }else if (App.Core.PerJing()<App.GetNumberParam("heal_below")){
+            App.Send("do 10 yun inspire")
         }else{
             App.Next()
             return
