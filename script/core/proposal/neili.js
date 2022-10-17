a632c80374b8182bb5f9466feb9f209260ca92ad
jarlyyn <jarlyyn@qq.com>
2022-10-18 00:58:20 +0800

    update

--- script/core/proposal/neili.js
+++ script/core/proposal/neili.js
@@ -1,30 +1,39 @@
 (function (App) {
-    let proposal=Include("core/proposal/proposal.js")
-    let Neili= function(){
-        proposal.call(this,"neili")
-        this.Submit=function(){
-            return (App.GetParamNeiliMin())>App.Data.HP["eff_neili"]
+    let proposal = Include("core/proposal/proposal.js")
+    let Neili = function () {
+        proposal.call(this, "neili")
+        this.Submit = function () {
+            let min = App.GetParamNeiliMin()
+            if (min > 0 && min < 1) {
+                return (App.Data.HP["eff_qixue"] / App.Data.HP["qixue"]) < min || (App.Data.HP["eff_jing"] / App.Data.HP["jing"]) < min
+            }
+            return min > App.Data.HP["eff_neili"]
         }
-        this.Execute=function(){
-            if (App.Core.Sleep.CanSleep()){
+        this.Execute = function () {
+            if (App.Core.Sleep.CanSleep()) {
                 App.Commands([
-                    App.NewCommand("to",App.Options.NewWalk(App.GetSleepRooms())),
+                    App.NewCommand("to", App.Options.NewWalk(App.GetSleepRooms())),
                     App.NewCommand("nobusy"),
                     App.NewCommand("sleep"),
                 ]).Push()
-            }else{
+            } else {
+                let min=App.GetParamNeiliMin()
                 App.Commands([
-                    App.NewCommand("to",App.Options.NewWalk(App.GetSleepRooms())),
+                    App.NewCommand("to", App.Options.NewWalk(App.GetSleepRooms())),
                     App.NewCommand("nobusy"),
-                    App.NewCommand("function",function(){
-                        if (App.Data.HP["eff_neili"]<0){
-                            App.Core.Dazuo()
+                    App.NewCommand("function", function () {
+                        if ( min>= 1) {
+                            if (App.Data.HP["eff_neili"] < 0) {
+                                App.Core.Dazuo()
+                            } else {
+                                App.Send("dazuo max")
+                            }
                         }else{
-                            App.Send("dazuo max")
+                            App.Send("#sail")
                         }
                         App.Next()
                     }),
-                    App.NewCommand("delay",1),
+                    App.NewCommand("delay", min>=1?1:5),
                     App.NewCommand("nobusy"),
                 ]).Push()
             }
